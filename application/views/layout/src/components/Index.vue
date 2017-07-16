0320820ce59a0d6998ec365ea852d6e8fb80e6d6
Socialist <woldman7@gmail.com>
2017-07-16 11:34:21 +0300

    add layout

---
+++ application/views/layout/src/components/Index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m9 l10">
+                <h2 class="header">Roboto 2.0</h2>
+            </div>
+        </div>
+    </div>
+</template>
